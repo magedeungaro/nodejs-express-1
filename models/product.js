b28a17f6ca2336112bf0102d36de158e56d8ef53
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-19 13:53:58 -0300

    refactored Product model logic

--- models/product.js
+++ models/product.js
@@ -3,29 +3,27 @@ const path = require('path')
 const rootDir = require('../helpers/path')
 const filePath = path.join(rootDir, 'data', 'products.json')
 
+const load = (callback) => {
+  fs.readFile(filePath, (err, content) => {
+    let products = []
+
+    if (!err) products = JSON.parse(content)
+    callback(products)
+  })
+}
 module.exports = class Product {
   constructor(title) {
     this.title = title
-    this.path = path.join(rootDir, 'data', 'products.json')
   }
 
   save() {
-    fs.readFile(filePath, (err, content) => {
-      let products = []
-
-      if (!err) products = JSON.parse(content)
-
+    load((products) => {
       products.push(this)
       fs.writeFile(filePath, JSON.stringify(products), err => console.log(err))
-    })
+    });
   }
 
   static all(callback) {
-    fs.readFile(filePath, (err, content) => {
-      let products = []
-
-      if (!err) products = JSON.parse(content)
-      callback(products)
-    })
+    load(callback)
   }
 }
